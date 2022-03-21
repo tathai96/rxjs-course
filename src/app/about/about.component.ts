2047f76f9b6df2ee5ebc5a04306e38b854040e85
Tathagata Chakraborty <tathai96@gmail.com>
2022-03-22 00:00:07 +0530

    section-1-lecture-8

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { fromEvent, interval, timer } from 'rxjs';
 
 @Component({
   selector: 'about',
@@ -10,6 +11,48 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    //using rxjs
+
+    //interval operator - emits 1,2 etc each second
+    // const interval$ = interval(1000);
+    // interval$.subscribe(val => console.log('stream 1 =>' + val));
+    // interval$.subscribe(val => console.log('stream 2 =>' + val));
+
+
+    //timer operator - emits 1,2 etc each second after 3 seconds
+    const interval$ = timer(3000, 1000);
+    const sub = interval$.subscribe(val => console.log('stream 1 =>' + val));
+
+    setTimeout(() => {
+      sub.unsubscribe()
+    }, 5000);
+    // interval$.subscribe(val => console.log('stream 2 =>' + val));
+
+    //fromEvent operator
+    const click$ = fromEvent(document, 'click');
+    click$.subscribe(
+      evt => console.log(evt), //emits click
+      err => console.log(err), //emits error
+      () => console.log('completed') //completed
+    );
+    // interval$.subscribe(val => console.log('stream 2 =>' + val));
+
+
+
+
+    //using standard js
+    // document.addEventListener('click', evt => {
+    //   console.log(evt);
+    //   setTimeout(() => {
+    //     console.log('finished...');
+    //     let counter = 0;
+    //     setInterval(() => {
+    //       console.log(counter);
+    //       counter++;
+    //     }, 1000);
+    //   }, 3000);
+    // });
+
   }
 
 }
